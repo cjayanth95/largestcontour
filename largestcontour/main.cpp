27a848120360206442c5871e2c5595b6fdc49ffb
Jayanth <cjayanth95@gmail.com>
2016-05-25 12:24:16 +0530

    Initial Commit

---
+++ largestcontour/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  largestcontour
+//
+//  Created by labuser on 25/05/16.
+//  Copyright © 2016 labuser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
